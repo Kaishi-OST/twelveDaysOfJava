032726e11b027ac15a912022aa69b3b670f2b666
Kaishi-OST <43288541+Kaishi-OST@users.noreply.github.com>
2018-09-20 21:49:39 -0500

    Add files via upload

---
+++ hOMEwORK/src/hOMEwORK/ProgectA.java
@@ -0,0 +1,210 @@
+package hOMEwORK;
+
+public class ProgectA {
+	public static void first_day() {
+		System.out.print("On the first");
+	}
+	public static void second_day() {
+		System.out.print("On the second");
+	}
+	public static void third_day() {
+		System.out.print("On the third");
+	}
+	public static void fourth_day() {
+		System.out.print("On the fourth");
+	}
+	public static void fifth_day() {
+		System.out.print("On the fifth");
+	}
+	public static void sixth_day() {
+		System.out.print("On the sixth");
+	}
+	public static void seventh_day() {
+		System.out.print("On the seventh");
+	}
+	public static void eighth_day() {
+		System.out.print("On the eighth");
+	}
+	public static void ninth_day() {
+		System.out.print("On the ninth");
+	}
+	public static void tenth_day() {
+		System.out.print("On the tenth");
+	}
+	public static void eleventh_day() {
+		System.out.print("On the elevinth");
+	}
+	public static void twelth_day() {
+		System.out.print("On the twelth");
+	}
+	public static void true_love() {
+		System.out.println(" day of Christmas,");
+		System.out.println(" My true love gave to me.");
+	}
+	public static void itemA() {
+		System.out.println("A partridge in a pear tree");
+	}
+	public static void itemB() {
+		System.out.println("Two turtle doves, and");
+	}
+	public static void itemC() {
+		System.out.println("Three french hens");
+	}
+	public static void itemD() {
+		System.out.println("Four colly birds");
+	}
+	public static void itemE() {
+		System.out.println("Five gold rings");
+	}
+	public static void itemF() {
+		System.out.println("Six geese a-laying");
+	}
+	public static void itemG() {
+		System.out.println("Seven swans a-swimming");
+	}
+	public static void itemH() {
+		System.out.println("Eight maids a-milking");
+	}
+	public static void itemI() {
+		System.out.println("Nine ladies dancing");
+	}
+	public static void itemJ() {
+		System.out.println("Ten lords a-leaping");
+	}
+	public static void itemK() {
+		System.out.println("Eleven pipers piping");
+	}
+	public static void itemL() {
+		System.out.println("Twelve drummers drumming");
+	}
+	public static void endl() {
+		System.out.println();
+	}
+	public static void main(String[] args) {
+		first_day();
+		true_love();
+		itemA();
+		endl();
+		
+		second_day();
+		true_love();
+		itemB();
+		itemA();
+		endl();
+		
+		third_day();
+		true_love();
+		itemC();
+		itemB();
+		itemA();
+		endl();
+		
+		fourth_day();
+		true_love();
+		itemD();
+		itemC();
+		itemB();
+		itemA();
+		endl();
+		
+		fifth_day();
+		true_love();
+		itemE();
+		itemD();
+		itemC();
+		itemB();
+		itemA();
+		endl();
+		
+		sixth_day();
+		true_love();
+		itemF();
+		itemE();
+		itemD();
+		itemC();
+		itemB();
+		itemA();
+		endl();
+		
+		seventh_day();
+		true_love();
+		itemG();
+		itemF();
+		itemE();
+		itemD();
+		itemC();
+		itemB();
+		itemA();
+		endl();
+		
+		eighth_day();
+		true_love();
+		itemH();
+		itemG();
+		itemF();
+		itemE();
+		itemD();
+		itemC();
+		itemB();
+		itemA();
+		endl();
+		
+		ninth_day();
+		true_love();
+		itemI();
+		itemH();
+		itemG();
+		itemF();
+		itemE();
+		itemD();
+		itemC();
+		itemB();
+		itemA();
+		endl();
+		
+		tenth_day();
+		true_love();
+		itemJ();
+		itemI();
+		itemH();
+		itemG();
+		itemF();
+		itemE();
+		itemD();
+		itemC();
+		itemB();
+		itemA();
+		endl();
+		
+		eleventh_day();
+		true_love();
+		itemK();
+		itemJ();
+		itemI();
+		itemH();
+		itemG();
+		itemF();
+		itemE();
+		itemD();
+		itemC();
+		itemB();
+		itemA();
+		endl();
+		
+		twelth_day();
+		true_love();
+		itemL();
+		itemK();
+		itemJ();
+		itemI();
+		itemH();
+		itemG();
+		itemF();
+		itemE();
+		itemD();
+		itemC();
+		itemB();
+		itemA();
+		endl();
+	}
+}
